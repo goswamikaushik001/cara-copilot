df9106d13c5fd87952c1bec2b85019148548f88d
Kaushik Goswami <kaushik.goswami@sattvabit.com>
2025-04-06 16:11:35 +0530

    fix: update paths for preload and main files in Electron configuration

--- src/electron/utils/pathResolver.ts
+++ src/electron/utils/pathResolver.ts
@@ -6,7 +6,7 @@ export const getPreloadPath = () => {
   return path.join(
     app.getAppPath(),
     isDev() ? "." : "..",
-    "/dist-electron/preload.cjs"
+    "/dist-electron/preload/preload.cjs"
   );
 };
 
